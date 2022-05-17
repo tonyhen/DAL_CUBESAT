9e1268984f15199cdc0faff7f2ae3eda46ea0f8e
toasterj <tonyhenderson11@hotmail.com>
2022-05-17 16:51:47 -0300

    -Added test sat and datafile

--- testSATASSEM_DataFile.m
+++ testSATASSEM_DataFile.m
@@ -19,52 +19,52 @@ smiData.RigidTransform(7).ID = "";
 
 %Translation Method - Cartesian
 %Rotation Method - Arbitrary Axis
-smiData.RigidTransform(1).translation = [0 -90 0];  % mm
+smiData.RigidTransform(1).translation = [93 101.35167126846056 -50.457978822976202];  % mm
 smiData.RigidTransform(1).angle = 2.0943951023931953;  % rad
-smiData.RigidTransform(1).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
-smiData.RigidTransform(1).ID = "B[Satellite frame C.1-1:-:Reaction Wheel A.1-1]";
+smiData.RigidTransform(1).axis = [-0.57735026918962584 -0.57735026918962584 0.57735026918962584];
+smiData.RigidTransform(1).ID = "B[TESTSAT-1:-:ReactionWheelTEST-7]";
 
 %Translation Method - Cartesian
 %Rotation Method - Arbitrary Axis
-smiData.RigidTransform(2).translation = [0 2.8421709430404007e-14 0];  % mm
+smiData.RigidTransform(2).translation = [0 0 0];  % mm
 smiData.RigidTransform(2).angle = 2.0943951023931953;  % rad
-smiData.RigidTransform(2).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
-smiData.RigidTransform(2).ID = "F[Satellite frame C.1-1:-:Reaction Wheel A.1-1]";
+smiData.RigidTransform(2).axis = [-0.57735026918962562 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(2).ID = "F[TESTSAT-1:-:ReactionWheelTEST-7]";
 
 %Translation Method - Cartesian
 %Rotation Method - Arbitrary Axis
-smiData.RigidTransform(3).translation = [-40.000000000000007 -49.999999999999986 0];  % mm
+smiData.RigidTransform(3).translation = [50 7 -50];  % mm
 smiData.RigidTransform(3).angle = 2.0943951023931953;  % rad
-smiData.RigidTransform(3).axis = [0.57735026918962584 0.57735026918962584 0.57735026918962584];
-smiData.RigidTransform(3).ID = "B[Satellite frame C.1-1:-:Reaction Wheel A.1-2]";
+smiData.RigidTransform(3).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(3).ID = "B[TESTSAT-1:-:ReactionWheelTEST-8]";
 
 %Translation Method - Cartesian
 %Rotation Method - Arbitrary Axis
-smiData.RigidTransform(4).translation = [1.5760825693910812e-13 -5.6843418860808002e-14 6.5116862291448487e-14];  % mm
-smiData.RigidTransform(4).angle = 2.0943951023931957;  % rad
-smiData.RigidTransform(4).axis = [-0.57735026918962584 -0.57735026918962573 -0.57735026918962573];
-smiData.RigidTransform(4).ID = "F[Satellite frame C.1-1:-:Reaction Wheel A.1-2]";
+smiData.RigidTransform(4).translation = [0 8.8817841970012523e-16 0];  % mm
+smiData.RigidTransform(4).angle = 2.0943951023931953;  % rad
+smiData.RigidTransform(4).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
+smiData.RigidTransform(4).ID = "F[TESTSAT-1:-:ReactionWheelTEST-8]";
 
 %Translation Method - Cartesian
 %Rotation Method - Arbitrary Axis
-smiData.RigidTransform(5).translation = [0 -50.236264988595984 -39.999999999999979];  % mm
-smiData.RigidTransform(5).angle = 0;  % rad
-smiData.RigidTransform(5).axis = [0 0 0];
-smiData.RigidTransform(5).ID = "B[Satellite frame C.1-1:-:Reaction Wheel A.1-3]";
+smiData.RigidTransform(5).translation = [52.421904505125397 126.43218946766055 -6];  % mm
+smiData.RigidTransform(5).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(5).axis = [1 0 0];
+smiData.RigidTransform(5).ID = "B[TESTSAT-1:-:ReactionWheelTEST-9]";
 
 %Translation Method - Cartesian
 %Rotation Method - Arbitrary Axis
-smiData.RigidTransform(6).translation = [1.7447947817456004e-14 0 3.6209891616131144e-14];  % mm
+smiData.RigidTransform(6).translation = [0 0 0];  % mm
 smiData.RigidTransform(6).angle = 2.0943951023931953;  % rad
 smiData.RigidTransform(6).axis = [-0.57735026918962584 -0.57735026918962584 -0.57735026918962584];
-smiData.RigidTransform(6).ID = "F[Satellite frame C.1-1:-:Reaction Wheel A.1-3]";
+smiData.RigidTransform(6).ID = "F[TESTSAT-1:-:ReactionWheelTEST-9]";
 
 %Translation Method - Cartesian
 %Rotation Method - Arbitrary Axis
-smiData.RigidTransform(7).translation = [217.58482061249151 256.88369320493001 286.2422556809816];  % mm
+smiData.RigidTransform(7).translation = [0 0 0];  % mm
 smiData.RigidTransform(7).angle = 0;  % rad
 smiData.RigidTransform(7).axis = [0 0 0];
-smiData.RigidTransform(7).ID = "RootGround[Satellite frame C.1-1]";
+smiData.RigidTransform(7).ID = "RootGround[TESTSAT-1]";
 
 
 %============= Solid =============%
@@ -81,23 +81,23 @@ smiData.Solid(2).ID = "";
 
 %Inertia Type - Custom
 %Visual Properties - Simple
-smiData.Solid(1).mass = 0.066501233291188747;  % kg
-smiData.Solid(1).CoM = [0 5 0];  % mm
-smiData.Solid(1).MoI = [13.588418669166231 26.068483450145983 13.588418669166233];  % kg*mm^2
+smiData.Solid(1).mass = 0.0047532296848813585;  % kg
+smiData.Solid(1).CoM = [0 4.5955056179775289 0];  % mm
+smiData.Solid(1).MoI = [0.080076158102165729 0.074796608692992636 0.080076158102165729];  % kg*mm^2
 smiData.Solid(1).PoI = [0 0 0];  % kg*mm^2
-smiData.Solid(1).color = [0.89803921568627454 0.91764705882352937 0.92941176470588238];
+smiData.Solid(1).color = [0.6470588235294118 0.51764705882352935 0];
 smiData.Solid(1).opacity = 1;
-smiData.Solid(1).ID = "Reaction Wheel A.1*:*默认";
+smiData.Solid(1).ID = "ReactionWheelTEST*:*Default";
 
 %Inertia Type - Custom
 %Visual Properties - Simple
-smiData.Solid(2).mass = 1.3299443768653654;  % kg
-smiData.Solid(2).CoM = [0.00078196153252672678 0.027214604968067671 0.00084090934323370766];  % m
-smiData.Solid(2).MoI = [0.019238511913440597 0.0027673744031270144 0.019590697884970944];  % kg*m^2
-smiData.Solid(2).PoI = [2.8648238062332045e-05 8.7451657366194824e-07 2.6049135011962518e-05];  % kg*m^2
+smiData.Solid(2).mass = 0.84126551433242602;  % kg
+smiData.Solid(2).CoM = [50.392556270286967 130.82249929311627 -49.937859849145504];  % mm
+smiData.Solid(2).MoI = [9613.6770704527735 2350.521165624451 9415.5373337780038];  % kg*mm^2
+smiData.Solid(2).PoI = [-0.95944068707751773 0.016396426734207662 -6.2945766752347732];  % kg*mm^2
 smiData.Solid(2).color = [0.89803921568627454 0.91764705882352937 0.92941176470588238];
 smiData.Solid(2).opacity = 1;
-smiData.Solid(2).ID = "Satellite frame C.1*:*默认";
+smiData.Solid(2).ID = "TESTSAT*:*Default";
 
 
 %============= Joint =============%
@@ -110,12 +110,12 @@ smiData.Solid(2).ID = "Satellite frame C.1*:*默认";
 smiData.RevoluteJoint(3).Rz.Pos = 0.0;
 smiData.RevoluteJoint(3).ID = "";
 
-smiData.RevoluteJoint(1).Rz.Pos = 0;  % deg
-smiData.RevoluteJoint(1).ID = "[Satellite frame C.1-1:-:Reaction Wheel A.1-1]";
+smiData.RevoluteJoint(1).Rz.Pos = -89.999999999999986;  % deg
+smiData.RevoluteJoint(1).ID = "[TESTSAT-1:-:ReactionWheelTEST-7]";
 
-smiData.RevoluteJoint(2).Rz.Pos = -157.55171915317325;  % deg
-smiData.RevoluteJoint(2).ID = "[Satellite frame C.1-1:-:Reaction Wheel A.1-2]";
+smiData.RevoluteJoint(2).Rz.Pos = 0;  % deg
+smiData.RevoluteJoint(2).ID = "[TESTSAT-1:-:ReactionWheelTEST-8]";
 
-smiData.RevoluteJoint(3).Rz.Pos = 109.27263615243461;  % deg
-smiData.RevoluteJoint(3).ID = "[Satellite frame C.1-1:-:Reaction Wheel A.1-3]";
+smiData.RevoluteJoint(3).Rz.Pos = -89.999999999999986;  % deg
+smiData.RevoluteJoint(3).ID = "[TESTSAT-1:-:ReactionWheelTEST-9]";
 
