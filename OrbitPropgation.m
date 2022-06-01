86038822ee20d31f5ce8b02889e74deedfc1a123
toasterj <tonyhenderson11@hotmail.com>
2022-06-01 20:27:33 -0300

    -Added orbit propogation table with magnetic data

--- OrbitPropgation.m
+++ OrbitPropgation.m
@@ -1,7 +1,7 @@
 mission.StartDate = datetime(2020, 11, 30, 22, 23, 24);
-mission.Duration  = mission.StartDate + hours(24);
-sampletime = 10;
-
+mission.Duration  = mission.StartDate + hours(3);
+sampletime = 1;
+dy = decyear(mission.StartDate);
 
 sc = satelliteScenario(mission.StartDate, mission.Duration, sampletime);
 
@@ -10,5 +10,13 @@ tle = 'ISS.tle';
 iss = satellite(sc, "ISS.tle");
 
 %v = satelliteScenarioViewer(sc);
+pos = states(iss,"CoordinateFrame","geographic");
 
-for i=1:NumRows
+for c = 1:10801
+    [xyz(:,c),H(:,c),D(:,c),I(:,c),F(:,c)] = wrldmagm(pos(3,c),pos(1,c),pos(2,c), dy,'2020');
+end
+
+%total is the combined position and magnetic data for a 3 hour length of
+%the space station,Position(1:3), Fieldvector (4:6),Horizontal Intensity(7),
+%Declination(8), Inclination(9), Total Intensity(10).
+total=cat(1,pos,xyz,H,D,I,F);
