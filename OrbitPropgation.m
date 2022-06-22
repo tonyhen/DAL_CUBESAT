4c752f3860a39b174e8505e18ae35c0b50a97700
toasterj <tonyhenderson11@hotmail.com>
2022-06-22 15:13:02 -0300

    Added the test assembly

--- OrbitPropgation.m
+++ OrbitPropgation.m
@@ -9,14 +9,15 @@ sc = satelliteScenario(mission.StartDate, mission.Duration, sampletime);
 tle = 'ISS.tle';
 iss = satellite(sc, "ISS.tle");
 
-%v = satelliteScenarioViewer(sc);
+v = satelliteScenarioViewer(sc);
 pos = states(iss,"CoordinateFrame","geographic");
-
+pos_ecef = states(iss,"CoordinateFrame","ecef");
 for c = 1:10801
     [xyz(:,c),H(:,c),D(:,c),I(:,c),F(:,c)] = wrldmagm(pos(3,c),pos(1,c),pos(2,c), dy,'2020');
 end
-
+ 
 %total is the combined position and magnetic data for a 3 hour length of
 %the space station,Position(1:3), Fieldvector (4:6),Horizontal Intensity(7),
 %Declination(8), Inclination(9), Total Intensity(10).
-total=cat(1,pos,xyz,H,D,I,F);
+total=cat(1,pos_ecef,xyz,H,D,I,F);
+
