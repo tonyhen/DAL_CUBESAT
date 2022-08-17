9b023c76b3b57aaa5a267451c1763af7c4207ede
tonyh <tonyh@192.168.2.58>
2022-08-17 18:58:44 -0300

    I have done many things

--- OrbitPropgation.m
+++ OrbitPropgation.m
@@ -1,27 +1,25 @@
+% Please make the number of days reasonable for your computers. Recommend
+% 30 
+
 mission.StartDate = datetime(2020, 11, 30, 22, 23, 24);
-mission.Duration  = mission.StartDate + days(700);
+mission.Duration  = mission.StartDate + days(365);
+% Sample time is in seconds 
 sampletime = 60;
 dy = decyear(mission.StartDate);
-
 sc = satelliteScenario(mission.StartDate, mission.Duration, sampletime);
 
-
+% TLE is the two line element file for the ISS 
 tle = 'ISS.tle';
 sat = satellite(sc, "ISS.tle");
 base = groundStation(sc, 44.6476, -63.5728);
 ac = access(sat, base);
+re = 6371000;
 
 %v = satelliteScenarioViewer(sc);
-intvls = accessIntervals(ac);
+%intvls = accessIntervals(ac);
 
-%pos = states(sat,"CoordinateFrame","geographic");
-%pos_ecef = states(sat,"CoordinateFrame","ecef");
-%for c = 1:10801
- %   [xyz(:,c),H(:,c),D(:,c),I(:,c),F(:,c)] = wrldmagm(pos(3,c),pos(1,c),pos(2,c), dy,'2020');
-%end
- 
-%total is the combined position and magnetic data for a 3 hour length of
-%the space station,Position(1:3), Fieldvector (4:6),Horizontal Intensity(7),
-%Declination(8), Inclination(9), Total Intensity(10).
-total=cat(1,pos_ecef,xyz,H,D,I,F);
+[pos,vel] = states(sat,"CoordinateFrame","ecef");
+[row,col] = size(pos);
+sun = zeros(3,col);
+
 
